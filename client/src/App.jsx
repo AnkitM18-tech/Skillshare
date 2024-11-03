1dba859861d0d94654034dca3bccb5fdc7f11df7
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-03 23:54:30 +0530

    client initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-red-500">Check</Button>
+    </>
+  );
+}
+
+export default App;
