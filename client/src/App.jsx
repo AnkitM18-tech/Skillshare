713ac659dc10a99b98820c03e04ccae066b4d292
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-06 01:45:17 +0530

    Instructor Student Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,52 @@
 import { Route, Routes } from "react-router-dom";
 import AuthPage from "./pages/auth";
+import ProtectRoute from "./components/protectedRoute";
+import { useContext } from "react";
+import { AuthContext } from "./context/authContext";
+import Dashboard from "./pages/instructor/Dashboard";
+import StudentViewLayout from "./components/studentView/StudentViewLayout";
+import Home from "./pages/student/Home";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
+  const { auth } = useContext(AuthContext);
   return (
     <>
       <Routes>
-        <Route path="/auth" element={<AuthPage />} />
+        <Route
+          path="/auth"
+          element={
+            <ProtectRoute
+              authenticated={auth.authenticate}
+              user={auth?.user}
+              element={<AuthPage />}
+            />
+          }
+        />
+        <Route
+          path="/instructor"
+          element={
+            <ProtectRoute
+              authenticated={auth.authenticate}
+              user={auth?.user}
+              element={<Dashboard />}
+            />
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <ProtectRoute
+              authenticated={auth.authenticate}
+              user={auth?.user}
+              element={<StudentViewLayout />}
+            />
+          }
+        >
+          <Route path="" element={<Home />} />
+          <Route path="home" element={<Home />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
