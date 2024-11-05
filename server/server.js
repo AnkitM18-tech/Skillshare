09d51bfee179606fb7630e1ea49496ff1d885b3a
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-05 16:51:21 +0530

    cors middleware

--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRouter = require("./routes/auth.routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-cors({
-  origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-});
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
@@ -22,6 +25,7 @@ mongoose
   .catch((e) => console.log("Error while connecting to MongoDB :", e));
 
 // routes configuration
+app.use("/auth", authRouter);
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
