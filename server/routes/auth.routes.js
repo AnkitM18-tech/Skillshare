4191c1d09e080a35696cf841226528773b551930
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-06 01:43:45 +0530

    returned statusCode from check-auth

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -7,7 +7,7 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/check-auth").get(authenticate, (req, res) => {
   const user = req.user;
-  res
+  return res
     .status(200)
     .json({ success: true, message: "User Authenticated", data: { user } });
 });
