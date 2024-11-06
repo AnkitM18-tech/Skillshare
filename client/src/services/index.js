e8a99bddaf9e704c86371e2f73f16ff37f817e83
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-07 00:54:52 +0530

    enhanced mediaUploadService method

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -17,3 +17,14 @@ export async function checkAuthService() {
   const { data } = await axiosInstance.get("/auth/check-auth");
   return data;
 }
+export async function mediaUploadService(formData, onProgressCallback) {
+  const { data } = await axiosInstance.post("/media/upload", formData, {
+    onUploadProgress: (progressEvent) => {
+      const percentCompleted = Math.round(
+        (progressEvent.loaded * 100) / progressEvent.total
+      );
+      onProgressCallback(percentCompleted);
+    },
+  });
+  return data;
+}
