61848a073fd3c7c9e3b54f4e48bcfbe4ca242e2b
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-10 17:36:41 +0530

    student context and provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/authContext";
 import InstructorProvider from "./context/instructorContext";
+import StudentProvider from "./context/studentContext";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
       <InstructorProvider>
-        <App />
+        <StudentProvider>
+          <App />
+        </StudentProvider>
       </InstructorProvider>
     </AuthProvider>
   </BrowserRouter>
