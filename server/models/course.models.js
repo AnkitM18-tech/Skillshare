3d41ac052020f0a1809d2c74f10814ce76f55128
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-13 01:02:42 +0530

    modified students array schema

--- server/models/course.models.js
+++ server/models/course.models.js
@@ -75,6 +75,7 @@ const CourseSchema = new mongoose.Schema({
       studentId: String,
       studentName: String,
       studentEmail: String,
+      paidAmount: Number,
     },
   ],
   curriculum: [LectureSchema],
